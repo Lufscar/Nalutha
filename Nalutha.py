9669145feb5f87124f5575f25d09eff107cd5fc4
natashagraifman <natasha.graifman@quintoandar.com.br>
2021-06-16 19:13:20 -0300

    changing main to support error list

--- Nalutha.py
+++ Nalutha.py
@@ -5,16 +5,28 @@ from MyVisitorSemantico import MyVisitorSemantico
 from MyVisitorGerador import MyVisitorGerador
 import sys
 
-def main(argv):
+def main(self,argv):
     ipt = FileStream(argv[1])
     lexer = NaluthaLexer(ipt)
     stream = CommonTokenStream(lexer)
     parser = NaluthaParser(stream)
     tree = parser.program()
+    if self.semantico(tree):
+        visitorGerador = MyVisitorGerador(argv[2])
+        visitorGerador.visit(tree)
+    else:
+        print("Falha na criação do arquivo de saída.")
+
+
+def semantico(self,tree):
     visitorSemantico = MyVisitorSemantico()
     visitorSemantico.visit(tree)
-    visitorGerador = MyVisitorGerador(argv[2])
-    visitorGerador.visit(tree)
+    for error in visitorSemantico.ComprasSemanticoUtils.errosSemanticos:
+        print(error)
+    if(visitorSemantico.ComprasSemanticoUtils.errosSemanticos.isEmpty()):
+        return True
+    return False
+
 
 if __name__ == '__main__':
     main(sys.argv)
